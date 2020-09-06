771ba5d9fa33f8cae2889ca6254aa9d09f64e9b1
nosremetnarg <grant.alex.emerson@gmail.com>
2020-09-06 18:00:33 -0500

    update secret and remove additional git

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.Secret,
+    secret: 'superSecret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
