32782f93a231fc41fc0b3c407e6b1dde3983eaf7
nosremetnarg <grant.alex.emerson@gmail.com>
2020-09-06 21:10:36 -0500

    rearrange server

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const routes = require('./controllers/');
 const path = require('path');
+const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
-const app = express();
-const PORT = process.env.PORT || 3001; // makes app compatible with Heroku. Runs and env port live and 3001 locally
 
 // This sets up HANDLEBARS.js HTML template engine
 const exphbs = require('express-handlebars');
@@ -26,21 +24,24 @@ const sess = {
         db: sequelize
     })
 };
-app.use(session(sess));
-// add in css stylesheet
+const app = express();
+const PORT = process.env.PORT || 3001; // makes app compatible with Heroku. Runs and env port live and 3001 locally
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => { // sequelize taking the models and connecting them to associated database tables
+sequelize.sync({ force: false }).then(() => { 
     app.listen(PORT, () => console.log('Now listening'));
 });
 // if force was set to true it would drop and re-create all the database tables on startup
