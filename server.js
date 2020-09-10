7b5d03883434146398a43737ffd3d52b70f72a15
nosremetnarg <grant.alex.emerson@gmail.com>
2020-09-10 08:52:26 -0500

    username and roommate now dynamic and populate when a user enters a room

--- server.js
+++ server.js
@@ -21,12 +21,18 @@ io.on('connection', socket => {
     socket.on('joinRoom', ({ username, room }) => {
         const user = userJoin(socket.id, username, room);
         socket.join(user.room);
+
         // Welcomes current user
         socket.emit('message', formatMessage(botName, 'Welcome to Ask Father Time'));
 
         //Broadcast when a user connects
         socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
 
+        // send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
     });
 
     // Listen for chatMessage
@@ -40,8 +46,15 @@ io.on('connection', socket => {
     socket.on('disconnect', () => {
         const user = userLeave(socket.id);
 
-        if(user) {
-            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+        if (user) {
+            io.to(user.room).emit('message',
+                formatMessage(botName, `${user.username} has left the chat`));
+
+            // send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
         }
     })
 
