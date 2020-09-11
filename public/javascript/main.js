35495bd8807117896bdfc629c4341102d634e293
nosremetnarg <64425473+nosremetnarg@users.noreply.github.com>
2020-09-11 10:30:58 -0500

    Revert "Live chat"

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,11 +1,8 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
-
 
 // Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
+const { username, room }  = Qs.parse(location.search, {
     ignoreQueryPrefix: true
 })
 
@@ -13,12 +10,6 @@ const socket = io();
 
 // Join chatroom
 socket.emit('joinRoom', { username, room })
-
-// Get room and users
-socket.on('roomUsers', ({ room, users }) => {
-    outputRoomName(room);
-    outputUsers(users);
-})
 
 // Message from Server
 socket.on('message', message => {
@@ -32,7 +23,7 @@ socket.on('message', message => {
 // Message submit
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
+    
     // get message text
     const msg = e.target.elements.msg.value;
 
@@ -54,15 +45,3 @@ function outputMessage(message) {
     </p>`
     document.querySelector('.chat-messages').appendChild(div);
 }
-
-// Add room name to DOM
-function outputRoomName(room) {
-    roomName.innerText = room;
-}
-
-// Add users to DOM
-function outputUsers(users) { 
-    userList.innerHTML = `
-    ${users.map(user => `<li>${user.username}</li>`).join('')}
-    `;
-} // use join method b/c users are an array and turn it into a string
